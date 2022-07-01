b5d9cafc1443985ef4911f39a02e2c6d61ec6ea2
João Andrade <joaovictorvieira.23@hotmail.com>
2022-07-01 14:40:07 -0300

    feat: component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 };
