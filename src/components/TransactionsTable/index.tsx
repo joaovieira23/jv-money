60a3e59560b8b885775dfaf4099756cb8e58da37
João Andrade <joaovictorvieira.23@hotmail.com>
2022-07-01 15:29:56 -0300

    feat: transactions table component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,36 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$ 12.000</td>
+                        
+                        <td>Desenvolvimento</td>
+                        <td>20/06/2022</td>
+                    </tr>
+
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$1.100</td>
+                        
+                        <td>Casa</td>
+                        <td>30/06/2022</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
+}
